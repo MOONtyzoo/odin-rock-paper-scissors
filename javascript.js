5b6e3a603f5ada5baec9f310511d71143663041c
MOONtyzoo <91515108+MOONtyzoo@users.noreply.github.com>
2024-07-02 07:50:35 -0700

    Add and test "getComputerChoice" function
    getComputerChoice returns the computer opponent's decision of
    "rock", "paper", or "scissors".
    
    In order to ensure that each move has an equal chance,
    I created "testComputerChoice" to run 3000 scenarios and tally the moves

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
 console.log("External .js file connected");
+
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random()*3)
+    let decision = ""
+    switch (randomChoice) {
+        case 0: decision = "rock"; break;
+        case 1: decision = "paper"; break;
+        case 2: decision = "scissors"; break;
+        default: "rock"
+    }
+    return decision
+}
+
+function testComputerChoice() {
+    let rocks = 0;
+    let papers = 0;
+    let scissors = 0;
+    for (let i = 1; i <= 3000; i++) {
+        let computerChoice = getComputerChoice();
+        if (computerChoice == "rock") rocks++;
+        if (computerChoice == "paper") papers++;
+        if (computerChoice == "scissors") scissors++;
+    }
+    console.log(`Rocks: ${rocks}\nPapers: ${papers}\nScissors: ${scissors}`)
+}
+
+testComputerChoice()
